1fe9ce8fd0c79fecdf7700a0f6053059fced6287
MelissaMacedo <melissamaced28@gmail.com>
2025-08-06 13:53:29 -0300

    setting debug as false

--- receitas_ai/settings.py
+++ receitas_ai/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+52&unq)opa13m$x6#c5tsmlx0@+js=zmri^2&5_!xu5)o(^do'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [".vercel.app", "eai-chef.vercel.app"]
 
